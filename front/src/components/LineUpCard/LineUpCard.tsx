b5b47e2e3b37af42f5394b2c7c190aa8c4497655
tlaprovitta <tlaprovitta@gmail.com>
2025-04-01 19:59:23 -0300

    NavBar & Started Home Structure

---
+++ front/src/components/LineUpCard/LineUpCard.tsx
@@ -0,0 +1,7 @@
+export default function LineUpCard () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ front/src/helpers/LineUp.ts
@@ -0,0 +1,42 @@
+export const lineUp = [
+    {
+        id: 1,
+        name: "Silvestre y La Naranja",
+        time: "22:00",
+        scenery: "Rock Pop",
+        colorOne: "",
+        colorTwo: "",
+    },
+    {
+        id: 2,
+        name: "Koino Yokan",
+        time: "22:00",
+        scenery: "Rock Pop",
+        colorOne: "",
+        colorTwo: "",
+    },
+    {
+        id: 3,
+        name: "El Kuelgue",
+        time: "22:00",
+        scenery: "Rock Pop",
+        colorOne: "",
+        colorTwo: "",
+    },
+    {
+        id: 4,
+        name: "El Zar",
+        time: "22:00",
+        scenery: "Rock Pop",
+        colorOne: "",
+        colorTwo: "",
+    },
+    {
+        id: 5,
+        name: "Zoe Gottusso",
+        time: "22:00",
+        scenery: "Rock Pop",
+        colorOne: "",
+        colorTwo: "",   
+    }
+]
