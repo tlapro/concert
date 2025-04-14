683168b244236506b65f0af8e918d490ef37abf6
tlaprovitta <tlaprovitta@gmail.com>
2025-04-14 16:51:14 -0300

    Swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { loggerGlobal } from './middlewares/logger.middleware';
 import { errorLogger } from './middlewares/errorLogger.middleware';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
   });
   app.use(loggerGlobal);
   app.use(errorLogger);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Argentina Rock - Backend')
+    .setDescription(
+      'Esta es una API de Argentina Rock, que fue elaborada con NestJs',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3001);
 }
